1207d322b2d5754f11dba2c5429d7532a226988f
Sonal Dixit <sonal@kapso.in>
2024-04-21 17:50:41 +0530

    minor comments

--- frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import App from "./App.jsx";
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import React from "react";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
--- frontend/src/states/states.jsx
+++
@@ -1,3 +0,0 @@
-import { atom } from 'jotai';
-
-export const heartDataAtom = atom(null);
